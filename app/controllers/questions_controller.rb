32ceaf485a562af8047e24df3db4609c05d7efee
Alicia Fiuza <aliciafiuzaa@gmail.com>
2020-10-01 20:53:47 +0100

    CSS done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,9 +4,10 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    if params[:answer] == "I am going to work"
+    @question = params[:question]
+    if @question == "I am going to work"
       @coach_answer = "Great!"
-    elsif params[:answer].include? "?"
+    elsif @question.include? "?"
       @coach_answer = "Silly question, get dressed and go to work!"
     else
       @coach_answer = "I don't care, get dressed and go to work!"
